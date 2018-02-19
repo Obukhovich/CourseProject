306afe06716e401cbac7c4322bd0b9d9479daf92
Arthur Obukhovich <arthur.obukhovich@gmail.com>
2018-02-19 17:34:08 +0300

    Connect calculator buttons with MainViewController.

--- Payment List/Payment List/MainViewController.swift
+++ Payment List/Payment List/MainViewController.swift
@@ -11,6 +11,30 @@ import UIKit
 
 class MainViewController: UIViewController {
     
+    @IBAction func buttonOne(_ sender: UIButton) {
+    }
+    @IBAction func buttonTwo(_ sender: UIButton) {
+    }
+    @IBAction func buttonThree(_ sender: UIButton) {
+    }
+    @IBAction func buttonFour(_ sender: UIButton) {
+    }
+    @IBAction func buttonFive(_ sender: UIButton) {
+    }
+    @IBAction func buttonSix(_ sender: UIButton) {
+    }
+    @IBAction func buttonSeven(_ sender: UIButton) {
+    }
+    @IBAction func buttonEight(_ sender: UIButton) {
+    }
+    @IBAction func buttonNine(_ sender: UIButton) {
+    }
+    @IBAction func buttonZero(_ sender: UIButton) {
+    }
+    @IBAction func buttonDot(_ sender: UIButton) {
+    }
+    @IBAction func buttonDelete(_ sender: UIButton) {
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
