0b264db20b855d432e99c1e8afe517db3d91d2e1
Arthur Obukhovich <arthur.obukhovich@gmail.com>
2018-02-23 02:40:23 +0300

    Added fields to SettingsViewController.

--- Payment List/Payment List/Screens/SettingsViewController.swift
+++ Payment List/Payment List/Screens/SettingsViewController.swift
@@ -9,8 +9,33 @@
 import Foundation
 import UIKit
 
-class SettingsViewController: UIViewController {
+class SettingsViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+  
+    @IBOutlet weak var settingsTableView: UITableView!
     
+    let fieldName = ["Edit expenses", "Set password", "Reset settings"]
+//    let share = [Int]()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        settingsTableView.delegate = self
+        settingsTableView.dataSource = self
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return fieldName.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = settingsTableView.dequeueReusableCell(withIdentifier: "cell")
+        
+        cell?.textLabel?.text = fieldName[indexPath.row]
+//        cell?.detailTextLabel?.text = "\(share[indexPath.row])"
+        
+        return cell!
+    }
 }
 
 
